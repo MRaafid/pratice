84c1f9411956424d6ec4bba53fbe21513a585426
zero <muhammadraafid50290@gmail.com>
2023-09-04 15:27:15 +0530

    js

---
+++ first.js
@@ -0,0 +1,11 @@
+console.log("VALUE OF PI IS: " +Math.PI)
+var temp = 12;
+console.log(temp)
+console.log((temp + 34) /2);
+console.log((temp - 5) *4);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+var user = 'Raafid' ;
+typeof user;
---
+++ string.js
@@ -0,0 +1,9 @@
+var name ='raafid'
+var age = 'twenty five'
+var qa = 'BE'
+var output = 'My name is ' +name + '\nI am ' +age +'years old' +'\nI have complted ' +qa
+console.log(output);
+var ot = `Name: ${name}
+Age: ${age}
+Qualification: ${qa}`;
+console.log(ot);
