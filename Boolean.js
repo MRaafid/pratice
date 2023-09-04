29c962dab5265a914895274efad15af555d7f614
zero <muhammadraafid50290@gmail.com>
2023-09-04 20:12:01 +0530

    js

---
+++ Boolean.js
@@ -0,0 +1,10 @@
+var rep = true;
+var down = false;
+var pay = true;
+var del = false;
+var message ='';
+var post = !!message;
+console.log(post)
+var messag ='hey';
+var pos = !!messag;
+console.log(pos)
--- string.js
+++ string.js
@@ -1,5 +1,5 @@
 var name ='raafid'
-var age = 'twenty five'
+var age = 'twenty five '
 var qa = 'BE'
 var output = 'My name is ' +name + '\nI am ' +age +'years old' +'\nI have complted ' +qa
 console.log(output);
@@ -7,3 +7,5 @@ var ot = `Name: ${name}
 Age: ${age}
 Qualification: ${qa}`;
 console.log(ot);
+var abc = `my name is ${name} i am ${age} years old. i have completed  ${qa} \u{1F642}`;
+console.log(abc);
