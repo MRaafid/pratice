54fc172be16cab21e583561f6907c0225451b862
zero <muhammadraafid50290@gmail.com>
2023-09-18 15:25:38 +0530

    Ternary Operator

---
+++ Ternary.js
@@ -0,0 +1,7 @@
+let a = 'female';
+let gender = a === 'male' ? 'Luffy' : ' Nami';
+console.log(gender);
+
+let x = 90;
+let sinx = x === 0 ? 0 : x === 30 ? 0.5 : x === 90 ? 1 : 'Error' ;
+console.log("sin(" +x +") = " +sinx);
